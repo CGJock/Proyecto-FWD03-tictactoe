1f7d9a250433989167f473628b840bbf255845e8
CGJock <cjocksanest@gmail.com>
2024-05-18 20:14:56 -0600

    implementacion de 2 jugadores y validacion de valores no repetidos

--- tictactoe.js
+++ tictactoe.js
@@ -9,151 +9,139 @@ let pos7 = document.getElementById("vectorPoscicion7");
 let pos8 = document.getElementById("vectorPoscicion8");
 let contenedores = document.getElementsByClassName("contendorHijo")
 console.log(contenedores)
-
-let turno = false
-
-
+//variable player que contiene un booleano para determinar el valor en innerhtml correspondiente al jugador
+let player = false
 
 
-
+//grupo de arrays que simulan ser una matriz
 let cuadricula =[
     [pos0, pos1, pos2],
     [pos3, pos4, pos5],
     [pos6, pos7, pos8]
 ]
-
-contenedores[0].addEventListener("click",function() {
-      if (turno === false) {
-         cuadricula[0][0].innerHTML = "x"
-         
-         if (turno === false) {
-            turno = true
-         } else if (turno === true) {
-            turno = false
-         }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Funcion que verifica los turnos de los jugadores, revisa el valor de player, y devuele el valor opuesto
+//se declara una variable que va ser igual a una funcion de esta manera es posible llamar la funcion despues
+const verificarPlayer = function(player) {//se puede declarar el parametro de la funcion o ser especificado al llamarla
+   if (player === false) {
+      return true
       
-      } else{
-         cuadricula[0][0].innerHTML = "o"
-         console.log(turno)
+   } else if(player === true) //se checa que se cambie el valor boolean de player si se cumple la condicion
+      return false
 }
-})
-
-
-contenedores[1].addEventListener("click", function () {
-   if (turno ===  false) {
-      cuadricula[0][1].innerHTML = "x"
-      if (turno === true) {
-         turno = false
-      } else if (turno === false) {
-         turno = true
-      }
-   } else{
-      cuadricula[0][1].innerHTML = "o"
-      console.log(turno)
-}
-   
-})
-
-
-contenedores[2].addEventListener("click", function () {
-   if (turno === false) {
-      cuadricula[0][2].innerText = "x"
-      if (turno === true) {
-         turno = false
-      } else if (turno === false) {
-         turno = true
-         console.log(turno)
-      }
-   } else{
-      cuadricula[0][2].innerText = "o"
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function verificarWiner(cuadricula) {
+   if (cuadricula[0][0],cuadricula[0][1],cuadricula[0][2].innerHTML == "x") {
+      return alert("el ganador es el jugador x")
+   } else if (cuadricula[0][0],cuadricula[0][1],cuadricula[0][2].innerHTML == "o") {
+      return alert("el ganador es el player 0")
    }
+}
+
+
+
+contenedores[0].addEventListener("click",function addSimbol() {
+   
+      if (player === false){
+         cuadricula[0][0].innerHTML = "x"
+      } else if(player === true){
+         cuadricula[0][0].innerHTML = "o"
+      }
+       player = verificarPlayer(player) //se llama la funcion player asignandole este nuevo valor a player
+       contenedores[0].removeEventListener("click", addSimbol)
+       verificarWiner(cuadricula)
+      
       
 })
-contenedores[3].addEventListener("click", function () {
-   if (turno === false){
+
+
+contenedores[1].addEventListener("click", function addSimbol() {
+   console.log(cuadricula[0][1].innerHTML)
+   if (player === false){
+      cuadricula[0][1].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[0][1].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[1].removeEventListener("click", addSimbol)
+   verificarWiner(cuadricula)
+})
+
+
+
+contenedores[2].addEventListener("click", function addSimbol() {
+   if (player === false){
+      cuadricula[0][2].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[0][2].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[2].removeEventListener("click", addSimbol)
+   verificarWiner(cuadricula)
+})
+
+contenedores[3].addEventListener("click", function addSimbol() {
+   if (player === false){
       cuadricula[1][0].innerHTML = "x"
-      turno = true
-   } else {
+   } else if(player === true){
       cuadricula[1][0].innerHTML = "o"
    }
+   player = verificarPlayer(player)
+   contenedores[3].removeEventListener("click", addSimbol)
+})
+
+contenedores[4].addEventListener("click", function addSimbol() {
+   if (player === false){
+      cuadricula[1][1].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[1][1].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[4].removeEventListener("click", addSimbol)
+})
+
+contenedores[5].addEventListener("click", function addSimbol() {
+   if (player === false){
+      cuadricula[1][2].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[1][2].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[5].removeEventListener("click", addSimbol)
       
 })
-contenedores[4].addEventListener("click", function () {
-      if (turno === false) {
-         cuadricula[1][1].innerHTML = "x"
-         turno = true
-      } else{
-         cuadricula[1][1].innerHTML = "o"
-      }
+contenedores[6].addEventListener("click", function addSimbol() {
+   if (player === false){
+      cuadricula[2][0].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[2][0].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[6].removeEventListener("click", addSimbol)
       
 })
-contenedores[5].addEventListener("click", function () {
-      if (turno === false) {
-         cuadricula[1][2].innerHTML = "x"
-         turno = true
-      } else{
-         cuadricula[1][2].innerHTML = "o"
-      }
-      
+contenedores[7].addEventListener("click", function addSimbol() {
+   if (player === false){
+      cuadricula[2][1].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[2][1].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[7].removeEventListener("click", addSimbol)
 })
-contenedores[6].addEventListener("click", function () {
-      if (turno === false) {
-         cuadricula[2][0].innerHTML = "x"
-         turno = true
-      } else{
-         cuadricula[2][0].innerHTML = "o"
-      }
-      
-})
-contenedores[7].addEventListener("click", function () {
-      if (turno === false) {
-         cuadricula[2][1].innerHTML = "x"
-         turno = true
-      } else {
-         cuadricula[2][1].innerHTML = "o"
-      }
-      
-})
-contenedores[8].addEventListener("click", function () {
-      if (turno === false) {
-         cuadricula[2][2].innerHTML = "x"
-      } else {
-         cuadricula[2][2].innerHTML = "o"
-      }
-      
+contenedores[8].addEventListener("click", function addSimbol() {
+   if (player === false){
+      cuadricula[2][2].innerHTML = "x"
+   } else if(player === true){
+      cuadricula[2][2].innerHTML = "o"
+   }
+   player = verificarPlayer(player)
+   contenedores[8].removeEventListener("click", addSimbol)
 })
  
 
 
 
 
-/*function addSimbol1() {
- p.innerHTML = "x"
- if (p.innerHTML = "x") {
-    cuadricula[0][0] = "x"
-    console.log(cuadricula)
- }
-}
-function addSimbol2() {
-    p2.innerHTML = "x"
-    if (p2.innerHTML = "x") {
-       cuadricula[1][0] = "x"
-       console.log(cuadricula)
-    }
-   }
-*/
-// function addSimbol1() {
-//    let  p = document.createElement ("p")
-//     p.innerHTML = "x"
-//     pos1.appendChild(p)
-
-//     if (p.innerHTML = "x") {
-//        cuadricula[0][0] = "x"}
-//     for (let i = 0; i < cuadricula.length; i++) {
-//         console.log(cuadricula[i])
-        
-//     }
-// }
-// console.log(cuadricula)
 
 
